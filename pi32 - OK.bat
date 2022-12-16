4c2826ec81db6877b43a77d3fb7e5f3a47fdd31f
jg3hlx <76939165+jg3hlx@users.noreply.github.com>
2022-12-16 19:06:02 +0900

    Add files via upload

---
+++ pi32 - OK.bat
@@ -0,0 +1,23 @@
+
+
+ 
+cd /d %~dp0
+ 
+"C:\Program Files\qemu\qemu-system-arm" ^
+-m 1024 ^
+-smp 4 ^
+-M raspi2b  ^
+-kernel kernel7.img ^
+-dtb bcm2709-rpi-2-b.dtb ^
+-drive format=raw,file=2022-09-22-raspios-bullseye-armhf-full-dm.img ^
+ -append "console=ttyAMA0 root=/dev/mmcblk0p2 rw rootwait rootfstype=ext4 dwc_otg.fiq_fsm_enable=0 bcm2708_fb.fbwidth=1280 bcm2708_fb.fbheight=800" ^
+-serial stdio ^
+-no-reboot ^
+-usb ^
+-device usb-host,vendorid=0403,productid=6001  ^
+-device usb-kbd ^
+-device usb-tablet ^
+-device usb-net,netdev=net0  ^
+-netdev user,id=net0
+
+
