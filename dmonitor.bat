1b7fba759e64b76f57761fbd48e056827c6a7fe6
jg3hlx <76939165+jg3hlx@users.noreply.github.com>
2022-12-18 06:53:24 +0900

    Update dmonitor.bat

--- dmonitor.bat
+++ dmonitor.bat
@@ -1,6 +1,3 @@
-
-
- 
 cd /d %~dp0
  
 "C:\Program Files\qemu\qemu-system-arm" ^
@@ -9,7 +6,7 @@ cd /d %~dp0
 -M raspi2b  ^
 -kernel kernel7.img ^
 -dtb bcm2709-rpi-2-b.dtb ^
--drive format=raw,file=2022-09-22-raspios-bullseye-armhf-full-dm.img ^
+-drive format=raw,file=2022-09-22-raspios-bullseye-armhf-full.img ^
  -append "console=ttyAMA0 root=/dev/mmcblk0p2 rw rootwait rootfstype=ext4 dwc_otg.fiq_fsm_enable=0 bcm2708_fb.fbwidth=1280 bcm2708_fb.fbheight=800" ^
 -serial stdio ^
 -no-reboot ^
@@ -19,5 +16,3 @@ cd /d %~dp0
 -device usb-tablet ^
 -device usb-net,netdev=net0  ^
 -netdev user,id=net0
-
-
